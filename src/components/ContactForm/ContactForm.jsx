b62f488575d220853509dda2872ae55ad3c93d71
margretheby <margretheby@users.noreply.github.com>
2023-09-26 10:46:34 +0200

    Update ContactForm with validation

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -8,24 +8,35 @@ function ContactForm() {
     const [ body, setBody ] = useState('')
 
     // Not finished
-    function SubmitMessage() {
-        if (onFormSubmit) {
-            return <div>Success!</div> 
-        } else {
-            return <div>Ooobs, something went wrong!</div>
-        }
+    function submitMessage() {
+        alert('SUCCESS! Your message was sent')
+        window.location.reload();
+        
+        return (
+        <div>
+            <h1>Success!</h1>
+        </div>
+        );
     }
 
     function onFormSubmit(event) {
         event.preventDefault();
-        if(onFormSubmit) {
-            return <SubmitMessage/>
+
+        const contactMessage = {
+            fullName,
+            subject,
+            email,
+            body
         }
+
+        console.log(contactMessage)
+        return submitMessage();
+        
     }
 
     function onTextInputChange(event) {
         const value = event.target.value;
-        if (event.target.name === 'first-name') {
+        if (event.target.name === 'full-name') {
             setFullName(event.target.value);
         }
         if (event.target.name === 'subject') {
@@ -34,7 +45,7 @@ function ContactForm() {
         if (event.target.name === 'email') {
             setEmail(event.target.value);
         }
-        if (event.target.name === 'subject') {
+        if (event.target.name === 'body') {
             setBody(event.target.value); 
         }
     }
@@ -43,17 +54,44 @@ function ContactForm() {
         <div className='bg-pink max-w-fit mt-10 mb-16'>
             <form onSubmit={onFormSubmit} className='flex flex-col py-3 px-5'>
                 <label htmlFor="full-name">Full name</label>
-                <input value={fullName} type="text" name="full-name" placeholder="Your full name" className='mb-4 p-1' required
-                onChange={onTextInputChange} />
+                <input 
+                    value={fullName} 
+                    type="text" 
+                    name="full-name" 
+                    minLength='3' 
+                    placeholder="Your full name" 
+                    className='mb-4 p-1' 
+                    required
+                    onChange={onTextInputChange} />
                 <label htmlFor="subject">Subject</label>
-                <input value={subject} type="text" name="subject" placeholder="Subject" className='mb-4 p-1' required
-                onChange={onTextInputChange} />
+                <input 
+                    value={subject} 
+                    type="text" 
+                    name="subject" 
+                    minLength='3' 
+                    placeholder="Subject" 
+                    className='mb-4 p-1' 
+                    required
+                    onChange={onTextInputChange} />
                 <label htmlFor="email">Email</label>
-                <input value={email} type="email" name="email" placeholder="Your email" className='mb-4 p-1' required
-                onChange={onTextInputChange} />
+                <input 
+                    value={email} 
+                    type="email" 
+                    name="email" 
+                    placeholder="Your email" 
+                    className='mb-4 p-1' 
+                    required
+                    onChange={onTextInputChange} />
                 <label htmlFor="body">Body</label>
-                <input value={body} type="text" name="body" placeholder="Your message" className='mb-2 p-1' required
-                onChange={onTextInputChange} />
+                <input 
+                    value={body} 
+                    type='text' 
+                    name="body" 
+                    minLength='3' 
+                    placeholder="Your message" 
+                    className='mb-2 p-1 h-16' 
+                    required
+                    onChange={onTextInputChange} />
                 <div className='flex justify-center'>
                     <button className='bg-black text-rose hover:bg-red hover:text-black px-6 py-2 mt-7 mb-6 font-semibold'>Submit form</button>
                 </div>
